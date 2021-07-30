9e6ce5b8eb38f55ca10013a58306e324f3933485
Carlos Henrique <carlosh.carmo@hotmail.com>
2021-07-30 09:16:53 -0300

    primeiro commit

---
+++ src/main/java/br/com/devs/Nexo/NexoApplication.java
@@ -0,0 +1,13 @@
+package br.com.devs.Nexo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devs/Nexo/NexoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devs.Nexo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
