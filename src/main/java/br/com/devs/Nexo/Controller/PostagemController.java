90ae1017fa0103e472c3d06a8332bd813cc12214
felipe0059 <felipe.nx4@gmail.com>
2021-09-01 15:43:44 -0300

    ajuste controller

--- src/main/java/br/com/devs/Nexo/Controller/PostagemController.java
+++ src/main/java/br/com/devs/Nexo/Controller/PostagemController.java
@@ -2,7 +2,6 @@ package br.com.devs.Nexo.Controller;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
